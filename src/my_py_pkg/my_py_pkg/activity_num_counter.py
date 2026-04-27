44dff2058478e4a6b4da342ee03d49509ccd2f02
Sujan K K <sujangowda28@gmail.com>
2026-04-27 19:10:45 +0530

    Counter Node Service Update

--- src/my_py_pkg/my_py_pkg/activity_num_counter.py
+++ src/my_py_pkg/my_py_pkg/activity_num_counter.py
@@ -2,14 +2,18 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
- 
+from example_interfaces.srv import SetBool
  
 class CounterNode(Node):  
     def __init__(self):
         super().__init__("number_counter")  
         self.counter_ = 0
-        self.number_count_ = self.create_publisher(Int64, "number_count", 10)
-        self.subscriber_ = self.create_subscription(Int64, "number", self.callback_number, 10)
+        self.number_count_ = self.create_publisher(
+            Int64, "number_count", 10)
+        self.subscriber_ = self.create_subscription(
+            Int64, "number", self.callback_number, 10)
+        self.reset_counter_service_ = self.create_service(
+            SetBool, "reset_counter", self.callback_reset_counter)
         self.get_logger().info("Number counter has been activated")
 
     def callback_number(self, msg: Int64):
@@ -17,6 +21,18 @@ class CounterNode(Node):
         new_msg = Int64()
         new_msg.data = self.counter_
         self.number_count_.publish(new_msg)
+
+    def callback_reset_counter(self, request: SetBool.Request, response: SetBool.Response):
+        if request.data:
+            self.counter_ = 0
+            response.success = True
+            response.message = "Counter has been reset to zero."
+            self.get_logger().info(response.message)
+        else:
+            response.success = False
+            response.message = "Counter reset failed. Request data was false."
+            self.get_logger().info(response.message)
+        return response
 
  
  
