b100de24c69ba868ca89acb3b8b3e36841585a77
sujan <sujangowda28@gmail.com>
2026-05-01 04:53:34 +0530

    Led Status

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -32,7 +32,8 @@ setup(
             "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
             "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main",
             "add_two_ints_client = my_py_pkg.add_two_ints_client:main",
-            "hw_status_publisher = my_py_pkg.hardware_status_publisher:main"
+            "hw_status_publisher = my_py_pkg.hardware_status_publisher:main",
+            "led_panel = my_py_pkg.led_panel:main"
         ],
     },
 )
