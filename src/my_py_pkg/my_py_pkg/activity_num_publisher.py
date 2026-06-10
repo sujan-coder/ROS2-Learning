93c715941522db0d73140bff989eaa7ea1711ebf
sujan <sujangowda28@gmail.com>
2026-06-10 19:27:41 +0530

    Xml Launch files added

--- src/my_py_pkg/my_py_pkg/activity_num_publisher.py
+++ src/my_py_pkg/my_py_pkg/activity_num_publisher.py
@@ -3,38 +3,36 @@ import rclpy
 from rclpy.node import Node
 from rclpy.parameter import Parameter
 from example_interfaces.msg import Int64
- 
-class PublisherNode(Node):
 
+class NumberPublisherNode(Node):
     def __init__(self):
-        super().__init__("number_publisher") 
+        super().__init__("number_publisher")
         self.declare_parameter("number", 2)
-        self.declare_parameter("time_period", 1.0)
-        
+        self.declare_parameter("timer_period", 1.0)
         self.number_ = self.get_parameter("number").value
-        self.time_period_ = self.get_parameter("time_period").value
+        self.timer_period_ = self.get_parameter("timer_period").value
         self.add_on_set_parameters_callback(self.parameters_callback)
 
-        self.publisher_ = self.create_publisher(Int64, "number", 10)
-        self.create_timer(self.time_period_, self.publish_number)
-        self.get_logger().info("Publisher Node has  Activated")
-    
+        self.number_publisher_ = self.create_publisher(Int64, "number", 10)
+        self.number_timer_ = self.create_timer(self.timer_period_, self.publish_number)
+        self.get_logger().info("Number publisher has been started.")
+
     def publish_number(self):
         msg = Int64()
         msg.data = self.number_
-        self.publisher_.publish(msg)
+        self.number_publisher_.publish(msg)
 
     def parameters_callback(self, params: list[Parameter]):
         for param in params:
             if param.name == "number":
                 self.number_ = param.value
- 
+
 def main(args=None):
     rclpy.init(args=args)
-    node = PublisherNode() 
+    node = NumberPublisherNode()
     rclpy.spin(node)
     rclpy.shutdown()
- 
- 
+
+
 if __name__ == "__main__":
     main()
