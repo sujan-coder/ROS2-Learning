71896460c22f15e8bd57d9883fbd5355ad0d800b
sujan <sujangowda28@gmail.com>
2026-06-13 02:18:30 +0530

    launch parameters via .py file

--- src/my_robot_bringup/launch/number_app.launch.py
+++ src/my_robot_bringup/launch/number_app.launch.py
@@ -8,7 +8,11 @@ def generate_launch_description():
         package="my_py_pkg",
         executable="number_publisher",
         name="my_number_publisher",
-        remappings=[("/number", "/my_number")]
+        remappings=[("/number", "/my_number")],
+        parameters=[
+            {"number": 12},
+            {"timer_period": 1.3}
+        ]
     )
 
     number_counter = Node(
