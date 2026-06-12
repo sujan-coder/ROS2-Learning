0b612a84a35009e8ef0a2e89d804d58cea9636e3
sujan <sujangowda28@gmail.com>
2026-06-13 02:25:47 +0530

    launch parameters via .py with yaml file

--- src/my_robot_bringup/launch/number_app.launch.py
+++ src/my_robot_bringup/launch/number_app.launch.py
@@ -1,18 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     ld = LaunchDescription()
+    
+    param_config = os.path.join(get_package_share_directory("my_robot_bringup"),
+                                "config", "number_app.yaml")
 
     number_publisher = Node(
         package="my_py_pkg",
         executable="number_publisher",
         name="my_number_publisher",
         remappings=[("/number", "/my_number")],
-        parameters=[
-            {"number": 12},
-            {"timer_period": 1.3}
-        ]
+        #parameters=[
+        #    {"number": 12},
+        #    {"timer_period": 1.3}
+        #]
+        parameters=[param_config]
+
     )
 
     number_counter = Node(
