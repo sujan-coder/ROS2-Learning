d2b1a18fbebacad276b80c90667a1f1b27fa9d3f
sujan <sujangowda28@gmail.com>
2026-08-10 19:47:01 +0530

    turtle Spawner updated

--- src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
+++ src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
@@ -3,12 +3,24 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.srv import Spawn
 from functools import partial
- 
+import random
+import math
  
 class TurtleSpawnerNode(Node): 
     def __init__(self):
         super().__init__("turtle_spawner")
+        self.turtle_name_prefix_ = "turtle"
+        self.turtle_counter_ = 0
         self.spawn_client_ = self.create_client(Spawn, "/spawn")
+        self.spawn_turtle_timer_ = self.create_timer(2.0, self.spawn_new_turtle)
+
+    def spawn_new_turtle(self):
+        self.turtle_counter_ += 1
+        name = self.turtle_name_prefix_ + str(self.turtle_counter_)
+        x = random.uniform(0.0, 11.0)
+        y = random.uniform(0.0, 11.0)
+        theta = random.uniform(0.0, 2*math.pi)
+        self.call_spawn_service(name, x, y, theta)
 
     def call_spawn_service(self, turtle_name, x, y, theta):
         while not self.spawn_client_.wait_for_service(1.0):
@@ -31,7 +43,6 @@ class TurtleSpawnerNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = TurtleSpawnerNode() 
-    node.call_spawn_service("test", 3.0, 3.0, 0.0)
     rclpy.spin(node)
     rclpy.shutdown()
  
