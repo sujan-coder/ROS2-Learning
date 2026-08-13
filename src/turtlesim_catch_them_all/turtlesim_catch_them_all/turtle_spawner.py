c8ad426f68fbabd0ae4b95d87504285bb10862eb
sujan <sujangowda28@gmail.com>
2026-08-13 20:11:55 +0530

    Fix catch service in spawner

--- src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
+++ src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
@@ -21,7 +21,7 @@ class TurtleSpawnerNode(Node):
         self.spawn_client_ = self.create_client(Spawn, "/spawn")
         self.kill_client_ = self.create_client(Kill, "/kill")
         self.catch_turtle_servvice_ = self.create_service(
-            CatchTurtle, "catch_turtle")
+            CatchTurtle, "catch_turtle", self.callback_catch_turtle)
         self.spawn_turtle_timer_ = self.create_timer(2.0, self.spawn_new_turtle)
 
     def callback_catch_turtle(self, request: CatchTurtle.Request, response: CatchTurtle.Response):
