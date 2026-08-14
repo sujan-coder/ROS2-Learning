18174268037d0834f863a91cc724ae584e965c83
sujan <sujangowda28@gmail.com>
2026-08-14 14:40:42 +0530

    Fix controller code

--- src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
+++ src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
@@ -8,12 +8,16 @@ from geometry_msgs.msg import Twist
 from my_robot_interfaces.msg import Turtle
 from my_robot_interfaces.msg import TurtleArray
 from my_robot_interfaces.srv import CatchTurtle
- 
+
 class TurtleControllerNode(Node):
     def __init__(self):
-        super().__init__("turtle_controller") 
-        self.turtle_to_catch_ : Turtle = None
-        self.pose_: Pose = None
+        super().__init__("turtle_controller")
+        self.declare_parameter("catch_closest_turtle_first", True)
+
+        self.catch_closest_turtle_first_ = self.get_parameter(
+            "catch_closest_turtle_first").value
+        self.turtle_to_catch_: Turtle = None
+        self.pose_: Pose = Nones
         self.cmd_vel_publisher_ = self.create_publisher(
             Twist, "/turtle1/cmd_vel", 10)
         self.pose_subscriber_ = self.create_subscription(
@@ -29,12 +33,25 @@ class TurtleControllerNode(Node):
 
     def callback_alive_turtles(self, msg: TurtleArray):
         if len(msg.turtles) > 0:
-            self.turtle_to_catch_ = msg.turtles[0]
+            if self.catch_closest_turtle_first_:
+                closest_turtle = None
+                closest_turtle_distance = None
+
+                for turtle in msg.turtles:
+                    dist_x = turtle.x - self.pose_.x
+                    dist_y = turtle.y - self.pose_.y
+                    distance = math.sqrt(dist_x * dist_x + dist_y * dist_y)
+                    if closest_turtle == None or distance < closest_turtle_distance:
+                        closest_turtle = turtle
+                        closest_turtle_distance = distance
+                self.turtle_to_catch_ = closest_turtle
+            else:
+                self.turtle_to_catch_ = msg.turtles[0]
 
     def control_loop(self):
         if self.pose_ == None or self.turtle_to_catch_ == None:
             return
-        
+
         dist_x = self.turtle_to_catch_.x - self.pose_.x
         dist_y = self.turtle_to_catch_.y - self.pose_.y
         distance = math.sqrt(dist_x * dist_x + dist_y * dist_y)
@@ -42,8 +59,8 @@ class TurtleControllerNode(Node):
         cmd = Twist()
 
         if distance > 0.5:
-            #position
-            cmd.linear.x = 2.0 *distance
+            # position
+            cmd.linear.x = 2*distance
 
             # orientation
             goal_theta = math.atan2(dist_y, dist_x)
@@ -52,9 +69,9 @@ class TurtleControllerNode(Node):
                 diff -= 2*math.pi
             elif diff < -math.pi:
                 diff += 2*math.pi
-            cmd.angular.z = 6.0 *diff
+            cmd.angular.z = 6*diff
         else:
-            #target reached
+            # target reached
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
             self.call_catch_turtle_service(self.turtle_to_catch_.name)
@@ -63,28 +80,27 @@ class TurtleControllerNode(Node):
         self.cmd_vel_publisher_.publish(cmd)
 
     def call_catch_turtle_service(self, turtle_name):
-        while not self.kill_client_.wait_for_service(1.0):
+        while not self.catch_turtle_client_.wait_for_service(1.0):
             self.get_logger().warn("Waiting for catch turtle service...")
         
-            request = CatchTurtle.Request()
-            request.name = turtle_name
-        
-            future = self.catch_turtle_client_.call_async(request)
-            future.add_done_callback(
-                partial(self.callback_catch_turtle_service, turtle_name=turtle_name))
+        request = CatchTurtle.Request()
+        request.name = turtle_name																					 
+        future = self.catch_turtle_client_.call_async(request)
+        future.add_done_callback(
+            partial(self.callback_call_catch_turtle_service, turtle_name=turtle_name))
 
-    def callback_catch_turtle_service(self, future, turtle_name):
+    def callback_call_catch_turtle_service(self, future, turtle_name):
         response: CatchTurtle.Response = future.result()
         if not response.success:
             self.get_logger().error("Turtle " + turtle_name + " could not be removed")
-      
+
  
 def main(args=None):
     rclpy.init(args=args)
     node = TurtleControllerNode()
     rclpy.spin(node)
     rclpy.shutdown()
- 
- 
+
+
 if __name__ == "__main__":
     main()
